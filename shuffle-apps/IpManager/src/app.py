0d45568b0b1911c0b69414d605bff511aa131c72
netsinfoo <netsinfoo@gmail.com>
2022-10-10 08:24:31 -0300

    Primeiro commit

---
+++ shuffle-apps/IpManager/src/app.py
@@ -0,0 +1,33 @@
+from phpipam_client import PhpIpamClient, GET, PATCH
+import sys
+import json
+import requests
+
+from walkoff_app_sdk.app_base import AppBase
+
+class IpManager(AppBase):
+    __verison__= " 0.0.2"
+    app_name = "IpManager"
+    
+    def __init__(self, redis, logger, console_logger=None):
+                
+        super().__init__(redis, logger, console_logger)
+        
+        
+    def get_info(self, client_ip, url, app_id, token):
+        
+        ipam = PhpIpamClient(url, app_id, token, encryption=True)
+                
+        consulta =  ipam.get('/addresses/search/'+ client_ip +'/')
+        
+        info = "Deu Certo"
+        
+        return info
+        
+if __name__ == "__main__":
+    IpManager.run()
+    
+    
+    
+    
+    
---
+++ shuffle-apps/IpManager/src/readme.md
@@ -0,0 +1,9 @@
+ url = 'http://ipmanager.info.ufrn.br/',
+            app_id='horus',
+            token='3ecbea6ab487e3d38ab1f8a50481ae2b',
+            encryption=True,)
+            
+url = "http://ipmanager.info.ufrn.br/"
+app_id ='horus'
+token = '3ecbea6ab487e3d38ab1f8a50481ae2b'
+ip = "10.3.225.5"
